b898926747e6d20175f03b02e48d72eab1686b04
wilson_wu <wevsmy@qq.com>
2018-05-31 20:52:21 +0800

    wilson-pr

---
+++ speedControl/client/client.py
@@ -0,0 +1,25 @@
+import requests
+
+def getHTMLText(url):
+    try:
+        data = requests.get(url, timeout = 30)
+        data.raise_for_status()
+        data.encoding = data.apparent_encoding
+        return data.text
+    except:
+        return ""
+
+
+def set(padel):
+    url = "http://127.0.0.1:5000/set?padel={}".format(int(padel))
+    data = getHTMLText(url)
+    print(data)
+
+
+def get():
+    url = "http://127.0.0.1:5000/get"
+    data = getHTMLText(url)
+    print(data)
+
+# set(3)
+get()
